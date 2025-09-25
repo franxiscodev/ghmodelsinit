c032682faf3ba315492b93c91cd0299bb2a48e47
FranxiscoDev <157801554+franxiscodev@users.noreply.github.com>
2025-09-25 21:24:57 +0200

    Create main.py
    base del inicio del proyecto

---
+++ main.py
@@ -0,0 +1,10 @@
+# main.py
+
+def saludar():
+    """
+    Esta función imprime un saludo simple en la consola.
+    """
+    print("¡Hola Mundo, desde Python!")
+
+if __name__ == "__main__":
+    saludar()
